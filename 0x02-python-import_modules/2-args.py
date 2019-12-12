b0eb30e5c60dd426fa88fe2d123e5884b9ca54cd
andres0191 <andres_felipe0191@hotmail.com>
2019-12-12 19:53:59 +0000

    add task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+if len(sys.argv) == 1:
+    print("{} arguments.".format(0))
+if len(sys.argv) == 2:
+    print("{}: argument:".format(1))
+    print("{}: {}".format(1, sys.argv[1]))
+else:
+    print("{}: arguments:".format(len(sys.argv)-1))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
