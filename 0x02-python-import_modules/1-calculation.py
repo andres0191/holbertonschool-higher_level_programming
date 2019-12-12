88b502016d22ca987c96665304b5f06c6b9d2e57
andres0191 <andres_felipe0191@hotmail.com>
2019-12-12 17:20:33 +0000

    add task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+from calculator_1 import add
+from calculator_1 import sub
+from calculator_1 import mul
+from calculator_1 import div
+a = 10
+b = 5
+add_0 = int(a) + int(b)
+res_0 = int(a) - int(b)
+mul_0 = int(a) * int(b)
+div_0 = int(a) // int(b)
+print("{} + {} = {}".format(a, b, add_0))
+print("{} - {} = {}".format(a, b, res_0))
+print("{} * {} = {}".format(a, b, mul_0))
+print("{} / {} = {}".format(a, b, div_0))
