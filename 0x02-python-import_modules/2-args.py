b610b92109490c77d63e30e18864c5f828708026
andres0191 <andres_felipe0191@hotmail.com>
2019-12-12 20:01:57 +0000

    modify contigion in task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,9 @@
 import sys
 if len(sys.argv) == 1:
     print("{} arguments.".format(0))
-if len(sys.argv) == 2:
-    print("{}: argument:".format(1))
-    print("{}: {}".format(1, sys.argv[1]))
+elif len(sys.argv) == 2:
+     print("{}: argument:".format(1))
+     print("{}: {}".format(1, sys.argv[1]))
 else:
     print("{}: arguments:".format(len(sys.argv)-1))
     for i in range(1, len(sys.argv)):
