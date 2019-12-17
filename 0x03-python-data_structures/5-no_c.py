afc5f7890fef3e8e44311afc3e90a6a90dc005bc
andres0191 <andres_felipe0191@hotmail.com>
2019-12-17 20:42:18 +0000

    add task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    if not my_string:
-        return(None)
     new_string = ""
     for i in range(len(my_string)):
         if my_string[i] == 'C' or my_string[i] == 'c':
