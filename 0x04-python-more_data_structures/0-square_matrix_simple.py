00ade20adc8b33b50e554fc6d4fb28dfac44eee3
andres0191 <andres_felipe0191@hotmail.com>
2019-12-19 17:15:02 +0000

    modify task 0 and 1

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     square = matrix.copy()
+    if not matrix:
+        return(None)
     for i in range(len(matrix)):
        square[i] = (list(map(lambda x: x**2, matrix[i])))
     return(square)
