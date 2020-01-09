645482f6776be627f9e040f15a933c7cd3176513
andres0191 <andres_felipe0191@hotmail.com>
2020-01-09 22:05:21 +0000

    modify task 3

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,9 +4,13 @@ class Square:
         private instance atribute size,
         and instantiation whit size
     """
-    def __init__(self, size):
+    def __init__(self, size = 0):
         """the paramethers of input
            are size, that is crucial
            for a square
         """
-       self.__size = size
+        if type(size) is not int:
+            raise TypeError ("size must be an integer")
+        elif size < 0:
+            raise ValueError ("size must be >= 0")
+        self.__size = size
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+class Square:
+    """"class Square defines a square by:
+        private instance atribute size,
+        and instantiation whit size
+    """
+    def __init__(self, size = 0):
+        """the paramethers of input
+           are size, that is crucial
+           for a square
+        """
+        if type(size) is not int:
+            raise TypeError ("size must be an integer")
+        elif size < 0:
+            raise ValueError ("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
+
