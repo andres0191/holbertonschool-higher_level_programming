26cd2581f5906d546bb7eabd47e6281f57ea9a6b
andres0191 <andres_felipe0191@hotmail.com>
2020-01-11 17:40:11 +0000

    modify task 0 and modify file of thest 0-add_integer.txt

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-class test():
-    def add_integer(a, b=89):
-        try:
-            a is not int or float
-            raise TypeError("a must be an integer")
-            b is not int or float
-            raise TypeError("b must be an integer")
-        except:
-            return a+b
+class Test():
+    pass
+"""Function that add a two integer"""
+
+
+def add_integer(a, b=89):
+    """add two int"""
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    c = int(a)+int(b)
+    return c
