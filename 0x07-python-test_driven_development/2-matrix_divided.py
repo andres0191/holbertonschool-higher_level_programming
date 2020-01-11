af17e9d0078013e294dd4f546ce087edb1f51f66
andres0191 <andres_felipe0191@hotmail.com>
2020-01-11 21:48:36 +0000

    modify task 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Function that divides all elements of a matrix """
 def matrix_divided(matrix, div):
-    new_matrix = matrix.copy()
+    new_matrix = []
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
     if div == 0:
@@ -9,13 +9,15 @@ def matrix_divided(matrix, div):
     if type(matrix) is not list:
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     for idx in range(len(matrix)):
-#        if matrix[idx] is not list:
-#            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-#        else:
-        for data_of_list in range(len(matrix[idx])):
-            if type(data_of_list) is not int and type(data_of_list) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            else:
-                result = round(data_of_list/div, 2)
-                new_matrix[idx][data_of_list] = result
+        if type(matrix[idx]) is not list:
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        else:
+            lista = []
+            for data_of_list in range(len(matrix[idx])):
+                if type(matrix[idx][data_of_list]) is not int and type(matrix[idx][data_of_list]) is not float:
+                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                else:
+                    result = round(matrix[idx][data_of_list]/div, 2)
+                    lista.append(result)
+            new_matrix.append(lista)
     return new_matrix
