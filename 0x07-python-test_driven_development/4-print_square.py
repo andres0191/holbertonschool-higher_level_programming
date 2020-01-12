95cb0f6915b759e5d048440f8354920c2c16ccb6
andres0191 <andres_felipe0191@hotmail.com>
2020-01-12 05:52:51 +0000

    modify task 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-def print_square(self, size=0):
+def print_square(size):
     if type(size) is not int:
         raise TypeError("size must be an integer")
     elif size < 0:
         raise ValueError("size must be >= 0")
     elif type(size) is float and size < 0:
         raise TypeError("size must be an integer")
-    for j in range(size):
-       print("#", end="")
-    print()
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
