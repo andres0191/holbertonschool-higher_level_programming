6cb72547461a95e31c256262a80b87c5bdc0e2e3
andres0191 <andres_felipe0191@hotmail.com>
2020-01-14 12:51:12 +0000

    modify task 0 and 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -9,6 +9,10 @@ def say_my_name(first_name, last_name=""):
     """
     function and cases of error
     """
+    if first_name is '\n' and last_name is '\n':
+        print("My name is \n\n"
+    if first_name is ' ' and last_name is ' ':
+        print("My name is    ")
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
