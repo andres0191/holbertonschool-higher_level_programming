1c963fefec699f8fd6e00d3de6e109b654252f61
andres0191 <andres_felipe0191@hotmail.com>
2020-01-14 01:04:16 +0000

    modify task 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+9#!/usr/bin/python3
 """Unittest for max_integer([..])
 """
 import unittest
@@ -10,6 +10,12 @@ class TestMaxInteger(unittest.TestCase):
         a = __import__('6-max_integer').__doc__
         self.assertTrue(len(a) > 1)
 
-    def case 2(self):
-        b = __import__('6-max_integer').__doc__
-        self.assertTrue(len(a) > 1)
+    def test_float(self):
+        self.assertEqual(max_integer([-3.23, 23.43, 44.2]), 44.2)
+
+    def test_string_list(self):
+        with self.assertRaises(TypeError):
+            max_integer('hello', 'there')
+
+    def test_string(self):
+        self.assertEqual(max_integer("hello"), 'o')
