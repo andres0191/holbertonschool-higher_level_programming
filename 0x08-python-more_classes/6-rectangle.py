afee4e19b40aca8c2c4373b43bb130f3adc828d0
andres0191 <andres_felipe0191@hotmail.com>
2020-01-15 16:24:09 +0000

    modify task 4, 5, 6, 7, 8, 9

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 class Rectangle():
     """class Rectangle that defines a rectangle """
-    number_of_instances = 0
-
     def __init__(self, width=0, height=0):
-        if type(width) is not int and type(height) is not int:
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-        if width < 0 or height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        self.__width = width
-        self.__height = height
-        self.__class__.number_of_instances += 1
+        self.__height = value
 
     @property
     def width(self):
@@ -22,21 +27,7 @@ class Rectangle():
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
-
-    @property
-    def height(self):
-        return (self.__height)
-
-    @width.setter
-    def height(self, value):
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__width = value
 
     def area(self):
         return (self.__width * self.__height)
