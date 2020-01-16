332bb018c8a696aa04548150679d7734662107b7
andres0191 <andres_felipe0191@hotmail.com>
2020-01-16 01:21:00 +0000

    pep8 style in file 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,6 @@
-#!/usr/bin/python3
-#for i in range(ord('z'), ord('a') -1, -1):
-for i in range(ord('z'), ord('a') -1, -1):
+#!/usr/bin/python
+for i in range(ord('z'), ord('a') - 1, - 1):
     j = 0
     if i % 2 == 1:
         j = -32
     print("{}".format(chr(i + j)), end="")
-
