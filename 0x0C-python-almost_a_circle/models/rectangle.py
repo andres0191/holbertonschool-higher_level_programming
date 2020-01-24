d24ee8acc9b45a282f96702b037a20d0185783b1
andres0191 <andres_felipe0191@hotmail.com>
2020-01-24 21:51:43 +0000

    add task 8 with main 7

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,7 +91,7 @@ class Rectangle(Base):
         return ("[Rectangle] ({}) {}\{} - {}/{}".format(self.id, self.x,
                 self.y, self.width, self.height))
     
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         if len(args) >= 1:
             self.id = args[0]
         if len(args) >= 2:
