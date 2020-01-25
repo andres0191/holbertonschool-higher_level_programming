03cde370c757b04c6859e4c4f440d36177544b4b
andres0191 <andres_felipe0191@hotmail.com>
2020-01-25 19:32:16 +0000

    add task 16 with main 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,3 +19,20 @@ class Base:
             return list_dictionaries
         else:
             return (json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        if list_objs is None:
+            empty_file = []
+            empty_file = cls.__name__
+            with open(empty_file, 'w') as file:
+                file.read([])
+        else:
+            new_list = []
+            for i in range(len(list_objs)):
+                new_doc = list_objs[i].__class__.__name__
+                new_doc += '.json'
+                new_list.append(list_objs[i].to_dictionary())
+                v1 = cls.to_json_string(new_list)
+            with open(new_doc, 'w') as file:
+                file.write(v1)
