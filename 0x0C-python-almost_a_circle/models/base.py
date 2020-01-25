8419ee414534718c2724e77cb9c2f01dbd1c083a
andres0191 <andres_felipe0191@hotmail.com>
2020-01-25 15:35:40 +0000

    add task 15 with main 14

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+import json
+
+
 class Base:
     """ The first class for the project almost a circle"""
     __nb_objects = 0
@@ -9,3 +12,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            list_dictionaries = []
+            return list_dictionaries
+        else:
+            return (json.dumps(list_dictionaries))
