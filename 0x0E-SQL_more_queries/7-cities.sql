6ec829b28354587b1b15969f63fccc20b50d3031
andres0191 <andres_felipe0191@hotmail.com>
2020-03-03 09:56:00 +0000

    add task 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- script that creates the database hbtn_0d_usa 
+-- and the table cities (in the database hbtn_0d_usa) 
+-- on your MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
+state_id INT FOREIGN KEY(state_id) REFERENCES states(id) NOT NULL,
+name VARCHAR(256) NOT NULL);
