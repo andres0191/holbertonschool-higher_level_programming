3e24ad2a81e86265697746730fd36286c5be9d4d
andres0191 <andres_felipe0191@hotmail.com>
2020-03-03 10:28:48 +0000

    modify task 8 and 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,7 @@
 -- script that lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name FROM cities ORDER BY cities.id ASC;
+
+-- The INNER JOIN keyword selects all rows from both tables as long as
+-- there is a match between the columns. If there are records in the "Orders"
+-- table that do not have matches in "Customers", these orders will not be shown!
+-- https://www.w3schools.com/sql/sql_join_inner.asp
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.states_id = states.id ORDER BY cities.id ASC;
