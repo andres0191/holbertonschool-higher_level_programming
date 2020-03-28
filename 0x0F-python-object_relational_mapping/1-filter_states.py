4042234671c94e3972c5ce143c4d9290c21336c9
andres0191 <andres_felipe0191@hotmail.com>
2020-03-28 20:08:31 +0000

    modify style in task 0, 1, 2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
 import MySQLdb
+# MySQLdb= interface for connecting to a MySQL.
 import sys
+# sys module provides access to any command-line arguments.
 
 
 if __name__ == "__main__":
+    #see if the module has been imported or not.
     if len(sys.argv):
+        #date of input
         username = sys.argv[1]
         password = sys.argv[2]
         database = sys.argv[3]
+    #date of input in db
     db = MySQLdb.connect(host='localhost',
                          user=username, passwd=password, db=database)
+    #var of save date of db.cursor
     cur = db.cursor()
+    #query for sql in python
     cur.execute("SELECT * FROM states
                 WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    #recorrer the date of db
     for row in cur.fetchall():
         print(row)
     # Close all cursors
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
 import MySQLdb
+# MySQLdb= interface for connecting to a MySQL.
 import sys
+# sys module provides access to any command-line arguments.
 
 
 if __name__ == "__main__":
+    #see if the module has been imported or not.
     if len(sys.argv):
+        #date of input
         username = sys.argv[1]
         password = sys.argv[2]
         database = sys.argv[3]
         name_of_state = sys.argv[4]
+    #date of input in db
     db = MySQLdb.connect(host='localhost',
                          user=username, passwd=password, db=database)
+    #var of save date of db.cursor
     cur = db.cursor()
+    #query for sql in python
     cur.execute("SELECT * FROM states
                 WHERE name='{}' ORDER BY states.id ASC".format(name_of_state,))
+    #recorrer the date of db
     for row in cur.fetchall():
         print(row)
     # Close all cursors
