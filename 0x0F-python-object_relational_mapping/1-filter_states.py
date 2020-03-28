d508dfd5563a31d29b468e2e20347a3a8a2e0b27
andres0191 <andres_felipe0191@hotmail.com>
2020-03-28 21:59:35 +0000

    modify task 1, 2, 3

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,25 +3,19 @@
     a name starting with N (upper N) from
     the database hbtn_0e_0_usa.
 """
-
-
 import MySQLdb
 import sys
-
-
 if __name__ == "__main__":
     """ see if the module has been imported or not. """
     if len(sys.argv):
         username = sys.argv[1]
         password = sys.argv[2]
         database = sys.argv[3]
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=username, passwd=password, db=database, charset=)
+    db = MySQLdb.connect(host='localhost',
+                         user=username, passwd=passwd, db=database)
     cur = db.cursor()
     cur.execute('''SELECT * FROM states
                 WHERE name LIKE 'N%' ORDER BY id ASC''')
     for row in cur.fetchall():
         if row[1][0] == 'N':
             print(row)
-    cur.close()
-    db.close()
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,35 +1,23 @@
 #!/usr/bin/python3
-""" script that takes in an argument
-    and displays all values in the states
-    table of hbtn_0e_0_usa where name
-    matches the argument.
 """
-
-
+script that list all states starting with N from database
+"""
 import MySQLdb
-""" MySQLdb= interface for
-    connecting to a MySQL.
-"""
 import sys
-""" sys module provides access
-    to any command-line arguments.
-"""
-
-
 if __name__ == "__main__":
-    if len(sys.argv):
+    """
+    main
+    """
+    if len(sys.argv) == 5:
         username = sys.argv[1]
-        password = sys.argv[2]
+        passwd = sys.argv[2]
         database = sys.argv[3]
-        name_of_state = sys.argv[4]
-        if name_of_state is not str:
-            exit()
+        argument = sys.argv[4]
     db = MySQLdb.connect(host='localhost',
-                         user=username, passwd=password, db=database)
+                         user=username, passwd=passwd, db=database)
     cur = db.cursor()
     cur.execute('''SELECT * FROM states
-                WHERE name='{}' ORDER BY id ASC'''.format(name_of_state))
-    for row in cur.fetchall():
-            print(row)
-    cur.close()
-    db.close()
+                 WHERE name = %s ORDER BY id ASC''', (argument,))
+    rows = cur.fetchall()
+    for state in rows:
+        print(state)
