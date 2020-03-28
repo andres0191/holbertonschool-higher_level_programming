66a574db4ece2592e24c81656c8a97ad7116dd6d
andres0191 <andres_felipe0191@hotmail.com>
2020-03-28 19:46:04 +0000

    add task 0, 1, 2, 3

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv):
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+    db = MySQLdb.connect(host='localhost',
+                         user=username, passwd=password, db=database)
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states ORDER BY states.id ASC')
+    for row in cur.fetchall():
+        print(row)
+    # Close all cursors
+    cur.close()
+    # Close all databases
+    db.close()
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv):
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+        name_of_state = sys.argv[4]
+    db = MySQLdb.connect(host='localhost', user=username,
+                         passwd=password, db=database)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states
+                WHERE name='{}' ORDER BY states.id ASC".format(name_of_state,))
+    for row in cur.fetchall():
+        print(row)
+    # Close all cursors
+    cur.close()
+    # Close all databases
+    db.close()
