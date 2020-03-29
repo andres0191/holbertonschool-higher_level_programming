ad0bc6ac1c640d53cbf143a5d77ca2ecd14d2a78
andres0191 <andres_felipe0191@hotmail.com>
2020-03-29 21:48:19 +0000

    modify task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,6 +11,9 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
