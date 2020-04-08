353571c4caec46884ff9f026e728d34759f6c5a7
andres0191 <andres_felipe0191@hotmail.com>
2020-04-08 14:33:20 -0500

    add task 6

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that makes a request to 0.0.0.0:5000/catch_me
-curl -sLH "You got me!" "0.0.0.0:5000/catch_me"
+curl -sLH "You got me!-User-Id:98" "0.0.0.0:5000/catch_me"
---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Write a function that finds a peak in a list of unsorted integers.
+
+def find_peak(list_of_integers):
+    for i in range(len(list_of_integers)):
+        if i[0] <= i[0+1]:
+            i[0+2]
+    return(i)
