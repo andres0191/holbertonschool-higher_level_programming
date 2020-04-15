85f6031e6a6fb011353d0f479b5e2e78f270abd2
andres0191 <andres_felipe0191@hotmail.com>
2020-04-15 12:45:44 -0500

    modify task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
     req = requests.get(url)
         print("Body response:")
-        print("\t- type: {}".format(type(str(html))))
+        print("\t- type: {}".format(type(req.text)))
         print("\t- utf8 content: {}".format(html.decode("utf-8")))
