d81332db1fe63392301c94e5525accdabc25cb9f
andres0191 <andres_felipe0191@hotmail.com>
2020-04-16 17:47:12 -0500

    add task 8:

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -11,6 +11,15 @@ if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
     letter = sys.argv[1]
     if len(letter) > 1:
-        print("No result")
-    r = post(url, data={'letter': letter}).json()
-
+        data={'letter': letter}    
+    else:
+        data = {'q': ""}
+    r = requests.post(url, data)
+    try:
+        r_json = r.json()
+        if len(r_json) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(r_json.get('id'), r_json.get('name')))
+    except:
+        print("Not a valid JSON")
