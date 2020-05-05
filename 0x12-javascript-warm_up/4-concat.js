747ed50eeff8889415ada72384f384ed2b76b691
andres0191 <andres_felipe0191@hotmail.com>
2020-05-05 22:01:07 +0000

    update task 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
 /* script that prints two arguments passed
 to it, in the following format */
-if (process.argv[2] === undefined) {
-  console.log('undefined is undefined');
-} else if (process.argv[2] === 'c' && process.argv[3] === 'cool') {
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
-} else if (process.argv[2] === 'c' && process.argv[3] === undefined) {
-  console.log(process.argv[2] + ' is undefined');
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
