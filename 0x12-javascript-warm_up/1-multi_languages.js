40ce546d37846f1c073b4cbc96bb9d1947b553c5
andres0191 <andres_felipe0191@hotmail.com>
2020-05-05 20:13:37 +0000

    modify task 0, 1, 2

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
-/*Write a script that prints 3 lines*/
-console.log("C is fun");
-console.log("Python is cool);
-console.log("Javascript is amazing");
+/* Write a script that prints 3 lines */
+console.log('C is fun\nPython is cool\nJavascript is amazing');
--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-/*Write a script that prints a message depending
-of the number of arguments passed*/
-if (process.argv[2] === undefined){
-console.log("No argument");}
-else{
-console.log("Argument found");}
+/* Write a script that prints a message depending
+of the number of arguments passed */
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log('Argument found');
+}
