ecbfd41fcca7f8a375b8187505bdf07b3b54feaa
andres0191 <andres_felipe0191@hotmail.com>
2020-05-05 21:17:41 +0000

    modify task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,6 +3,8 @@
 of the number of arguments passed */
 if (process.argv[2] === undefined) {
   console.log('No argument');
+} else if (process.argv[3]) {
+  console.log('Arguments found');
 } else {
   console.log('Argument found');
 }
