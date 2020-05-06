06a682d415f27c2e010c8575878b903659aa9e97
andres0191 <andres_felipe0191@hotmail.com>
2020-05-06 04:33:36 +0000

    uppdate task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 /* script that searches the second biggest
 integer in the list of arguments */
 const a = process.argv.length;
-if (a > 3) {
+if (a >= 3) {
   console.log(process.argv.sort().reverse()[1]);
 } else {
   console.log('0');
