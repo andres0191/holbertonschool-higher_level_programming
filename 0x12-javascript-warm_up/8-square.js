4667fa4f45100ad31afb5b3aa10e4613f1451ef8
andres0191 <andres_felipe0191@hotmail.com>
2020-05-06 00:39:35 +0000

    update task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* Write a script that prints a square */
 let i = 0;
-if (Number.isNaN(parseInt(process.argv[2] === undefined))) {
+if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing size');
 } for (i = 0; i < parseInt(process.argv[2]); i++) {
   console.log('x'.repeat(parseInt(process.argv[2])));
