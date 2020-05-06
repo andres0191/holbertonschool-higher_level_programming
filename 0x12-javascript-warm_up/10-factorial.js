24241afd2a78aec318ffb2930ce80725b661c54b
andres0191 <andres_felipe0191@hotmail.com>
2020-05-06 01:54:20 +0000

    add task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*script that computes and prints a factorial*/
+const a = parseInt(process.argv[2]);
+function factorial (a) {
+ return (a != 1) ? a * factorial(a - 1) : 1;
+}
+if (process.argv[2] === undefined) {
+  console.log('1');
+} else {
+  console.log(factorial(a));
+}
