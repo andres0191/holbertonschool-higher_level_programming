a60ca0a09c86b36c84ed4427a55b74296134a2ea
andres0191 <andres_felipe0191@hotmail.com>
2020-05-12 22:25:45 +0000

    update task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-/* Write a script that prints the title of a Star Wars*/
-let request = require('request');
-request.get('https://swapi-api.hbtn.io/api/films/:' + process.argv[2] + '/', function (error, response, body) {
+/* Write a script that prints the title of a Star Wars movie where the episode number matches a given integer. */
+const request = require('request');
+const myurl = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request.get(myurl + '/', function (error, response, body) {
   if (error) {
     console.log(error);
   } else if (response.statusCode === 200) {
