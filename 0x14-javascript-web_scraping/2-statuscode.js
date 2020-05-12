1165f5554af7f19073805beb2e1edda8851fe1d9
andres0191 <andres_felipe0191@hotmail.com>
2020-05-12 20:12:11 +0000

    update task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 /* write a script that display the status code of a GET request8 */
 const request = require('request');
-request(process.argv[2], function (error, response) {
+const myurl = process.argv[2];
+request(myurl, function (error, response) {
   console.log('code:', response && response.statusCode);
 });
