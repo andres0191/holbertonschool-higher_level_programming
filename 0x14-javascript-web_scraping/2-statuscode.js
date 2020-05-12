9082025c09da105c0dce660c59c8faaaeff8aa58
andres0191 <andres_felipe0191@hotmail.com>
2020-05-12 18:34:33 +0000

    update task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /* write a script that display the status code of a GET request8 */
-var https = require('https');
+const https = require('https');
 const url = process.argv[2];
 https.get(url, function (res) {
   console.log('code:', res.statusCode);
