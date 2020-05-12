43811b1f9690dbc99a7c4995d18c102002e14938
andres0191 <andres_felipe0191@hotmail.com>
2020-05-12 01:54:09 +0000

    change condition in line 5 > but >= for add the last item of the array

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 /* Write a function that returns the list in reverse */
 exports.esrever = function (list) {
   let i = 0; const arr = [];
-  for (i = list.length - 1; i > 0; i--) {
+  for (i = list.length - 1; i >= 0; i--) {
     arr.push(list[i]);
   }
   return (arr);
