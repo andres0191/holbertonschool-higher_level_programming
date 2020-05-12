a58ffb6805d86d119c7563e4fb73118a70e38b4b
andres0191 <andres_felipe0191@hotmail.com>
2020-05-12 01:48:26 +0000

    update task 8

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* Write a function that returns the list in reverse */
+exports.esrever = function (list) {
+  let i = 0; const arr = [];
+  for (i = list.length - 1; i > 0; i--) {
+    arr.push(list[i]);
+  }
+  return (arr);
+};
