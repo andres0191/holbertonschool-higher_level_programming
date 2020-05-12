ee9b86af9d956c647ee74d102851fb9025d48045
andres0191 <andres_felipe0191@hotmail.com>
2020-05-12 21:42:12 +0000

    update task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 /* Write a script that prints the title of a Star Wars*/
 let request = require('request');
-let myurl = 'https://swapi-api.hbtn.io/api/films/:'
-request.get(myurl + process.argv[2] + '/', function (error, response, body) {
+request.get('https://swapi-api.hbtn.io/api/films/:' + process.argv[2] + '/', function (error, response, body) {
   if (error) {
     console.log(error);
   } else if (response.statusCode === 200) {
