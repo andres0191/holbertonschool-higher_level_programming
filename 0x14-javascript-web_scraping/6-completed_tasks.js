64fa97219c4bca82b9e9a411fd58bcca9e760aa9
andres0191 <andres_felipe0191@hotmail.com>
2020-05-14 01:04:49 +0000

    update task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,11 +1,16 @@
 #!/usr/bin/node
-/* Write a script that computes the number of tasks completed by user id.*/
-const request = require('request'); const url = process.argv[2]+'?completed=true'; const i = 0;
+/* Write a script that computes the number of tasks completed by user id. */
+const request = require('request'); const url = process.argv[2] + '?completed=true'; const obj = Object();
 request.get(url, function (error, response, body) {
-   if (error){
-console.log(error);
-} for(i of JSON.parse(body)){
-if (i.completed === true){
-}
-
+  if (error) {
+    console.log(error);
+  } const json = JSON.parse(body);
+  for (const job of json) {
+    if (Object.prototype.hasOwnProperty.call(obj, job.userId)) {
+      obj[job.userId]++;
+    } else {
+      obj[job.userId] = 1;
+    }
+  }
+  console.log(obj);
 });
